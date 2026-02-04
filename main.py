4a34c35952220432187e891909cee0aa123424ad
fayozbekinformatika <154403070+fayozbekinformatika@users.noreply.github.com>
2026-02-04 13:23:16 +0300

    Refactor main.py for improved bot simulation

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def simulate_bot(request: SimulationRequest):
             messages.append({"role": role, "content": m['text']})
 
         response = client.chat.completions.create(
-            model="deepseek/deepseek-r1:free", # Yoki: google/gemini-2.0-flash-lite-preview-02-05:free
+            model="deepseek/deepseek-r1-0528:free", # Yoki: google/gemini-2.0-flash-lite-preview-02-05:free
             messages=messages,
             temperature=0
         )
@@ -86,3 +86,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     # Ekranda 127.0.0.1:8000 manzilini ochishingiz mumkin
     uvicorn.run(app, host="0.0.0.0", port=port)
+
